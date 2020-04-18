7d9c6a865d168acd6c95d217aaf08fc094dae9c4
parkinan <canthonyparkinson@shaw.ca>
2020-04-18 14:18:22 -0600

    Console mode working

--- csharp/MorseMapper.cs
+++ csharp/MorseMapper.cs
@@ -8,75 +8,75 @@ namespace ca.canthonyparkinson.gpio.morse {
                 case 'A':
                     return new List<Morse> {Morse.DOT, Morse.DASH };
                 case 'B':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DOT, Morse.DOT };
                 case 'C':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DASH, Morse.DOT };
                 case 'D':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DOT };
                 case 'E':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT };
                 case 'F':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DASH, Morse.DOT };
                 case 'G':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DOT };
                 case 'H':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DOT, Morse.DOT };
                 case 'I':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT };
                 case 'J':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DASH, Morse.DASH, Morse.DASH };
                 case 'K':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DASH };
                 case 'L':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DASH, Morse.DOT, Morse.DOT };
                 case 'M':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH };
                 case 'N':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT };
                 case 'O':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DASH };
                 case 'P':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DASH, Morse.DASH, Morse.DOT };
                 case 'Q':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DOT, Morse.DASH };
                 case 'R':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DASH, Morse.DOT };
                 case 'S':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DOT };
                 case 'T':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH };
                 case 'U':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DASH };
                 case 'V':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DOT, Morse.DASH };
                 case 'W':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DASH, Morse.DASH };
                 case 'X':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DOT, Morse.DASH };
                 case 'Y':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DASH, Morse.DASH };
                 case 'Z':
-                    return new List<Morse> { };
+                    return new List<Morse> { Morse.DASH, Morse.DASH, Morse.DOT, Morse.DOT};
                 case '0':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DASH, Morse.DASH, Morse.DASH };
                 case '1':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DASH, Morse.DASH, Morse.DASH, Morse.DASH };
                 case '2':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DASH, Morse.DASH, Morse.DASH };
                 case '3':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DOT, Morse.DASH, Morse.DASH };
                 case '4':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DOT, Morse.DOT, Morse.DASH };
                 case '5':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DOT, Morse.DOT, Morse.DOT, Morse.DOT, Morse.DOT };
                 case '6':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DOT, Morse.DOT, Morse.DOT, Morse.DOT };
                 case '7':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DOT, Morse.DOT, Morse.DOT };
                 case '8':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DASH, Morse.DOT, Morse.DOT };
                 case '9':
-                    return new List<Morse> { };
+                    return new List<Morse> {Morse.DASH, Morse.DASH, Morse.DASH, Morse.DASH, Morse.DOT };
                 case ' ':
                     return new List<Morse> { Morse.INTER_WORD_GAP};
                 default:
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,9 +1,50 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace ca.canthonyparkinson.gpio.morse {
     class Program {
         static void Main (string[] args) {
+            String msg = String.Join(' ', args);
+            process(msg);
+
+        }
+
+        static void process(String msg) {
+            foreach (char c in msg.ToCharArray()) {
+                process(c);
+            }
+        }
+
+        static void process(char c) {
+            IList<Morse> code = MorseMapper.Map(c);
+            if (code?.Count > 0) {
+                foreach (Morse m in code) {
+                    output(m);
+                }
+                output(Morse.INTER_LETTER_GAP);
+            } else {
+                if (c == ' ') {
+                    output(Morse.INTER_WORD_GAP);
+                }
+            }
+        }
+
+        static void output(Morse m) {
+            switch (m) {
+                case Morse.DOT:
+                    Console.Write('.');
+                    break;
+                case Morse.DASH:
+                    Console.Write('-');
+                    break;
+                case Morse.INTER_LETTER_GAP:
+                    Console.Write(' ');
+                    break;
+                case Morse.INTER_WORD_GAP:
+                    Console.Write("   ");
+                    break;
+            }
         }
     }
 }
